3c4ae85c011b3565e36daf2c70f35c6165f4e3bc
c)sc(o <69974944+chqyhslwszds@users.noreply.github.com>
2021-01-02 10:37:00 +0800

    Update resource.go

--- pkg/util/vhost/resource.go
+++ pkg/util/vhost/resource.go
@@ -29,24 +29,51 @@ var (
 
 const (
 	NotFound = `<!DOCTYPE html>
+
 <html>
-<head>
-<title>Not Found</title>
-<style>
-    body {
-        width: 35em;
-        margin: 0 auto;
-        font-family: Tahoma, Verdana, Arial, sans-serif;
-    }
-</style>
-</head>
-<body>
-<h1>The page you requested was not found.</h1>
-<p>Sorry, the page you are looking for is currently unavailable.<br/>
-Please try again later.</p>
-<p>The server is powered by <a href="https://github.com/fatedier/frp">frp</a>.</p>
-<p><em>Faithfully yours, frp.</em></p>
-</body>
+
+<head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"><title>404 找不到此页面/(ㄒoㄒ)/~~</title><meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"><meta http-equiv="X-UA-Compatible" content="ie=edge"><style>*{padding:0;margin:0}a{text-decoration:none}.notfoud-container .img-404{height:155px;background:url(http://www.wlphp.com/img/frp_404.png) center center no-repeat;-webkit-background-size:150px auto;margin-top:40px;margin-bottom:20px}.notfoud-container .notfound-p{line-height:22px;font-size:17px;padding-bottom:15px;border-bottom:1px solid #f6f6f6;text-align:center;color:#262b31}.notfoud-container .notfound-reason{color:#9ca4ac;font-size:13px;line-height:13px;text-align:left;width:210px;margin:0 auto}.notfoud-container .notfound-reason p{margin-top:13px}.notfoud-container .notfound-reason ul li{margin-top:10px;margin-left:36px}.notfoud-container .notfound-btn-container{margin:40px auto 0;text-align:center}.notfoud-container .notfound-btn-container .notfound-btn{display:inline-block;border:1px solid #ebedef;background-color:#239bf0;color:#fff;font-size:15px;border-radius:5px;text-align:center;padding:10px;line-height:16px;white-space:nowrap}</style></head><body>
+
+
+
+    <div class="notfoud-container">
+
+        <div class="https://pic.iris4.com/n.png">
+
+        </div>
+
+        <p class="notfound-p">哎呀迷路了...</p>
+
+        <div class="notfound-reason">
+
+            <p>可能的原因：</p>
+
+            <ul>
+
+                <li>frpc没有启动成功</li>
+
+                <li>域名不正确</li>
+
+                <li>内网端不存在该页面</li>
+
+                <li>我们的服务器被外星人劫持了</li>
+
+            </ul>
+
+        </div>
+
+        
+
+        <div class="notfound-reason">
+
+            <p align="center"><a href="http://www.iris4.com/">公益内网反代服务</a>
+
+        </div>
+
+
+
+    </div></body>
+
 </html>
 `
 )
